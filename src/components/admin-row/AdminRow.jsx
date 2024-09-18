8a344da3f862cf36290250d55517ae7683d8c4e9
ferchu <f3rn4nd4.2022@gmail.com>
2024-09-18 17:12:04 -0300

    tabla admin product

--- src/components/admin-row/AdminRow.jsx
+++ src/components/admin-row/AdminRow.jsx
@@ -1,7 +1,18 @@
+import './adminRow.css'
 
-
-export default function AdminRow() {
+export default function AdminRow(product) {
   return (
-    <div>AdminRow</div>
+    <tr className="admin_table_row">
+      <td><div className="image"><img src={product.image} alt={product.name} /></div></td>
+      <td><div className="name">{product.name}</div></td>
+      <td><div className="description_container">{product.description}</div></td>
+      <td><div className="price">{product.price}</div></td>
+      <td><div className="category">{product.category}</div></td>
+      <td><div className="date">{product.createdAt}</div></td>
+      <td><div className="actions_container">
+        <button>Editar</button>
+        <button>Eliminar</button>
+      </div></td>
+    </tr>
   )
 }
--- src/components/admin-table/AdminTable.jsx
+++ src/components/admin-table/AdminTable.jsx
@@ -1,12 +1,25 @@
-
+import AdminRow from '../admin-row/AdminRow';
+import './adminTable.css'
 
 export default function AdminTable({products}) {
   return (
     <table className="admin_table">
+      <thead>
+        <tr>
+          <th>Imagen</th>
+          <th>Nombre producto</th>
+          <th>Descripcion</th>
+          <th>Precio</th>
+          <th>Categoria</th>
+          <th>Fecha de Ingreso</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
         {products.map (eachProd => {
-          return<h2 key={eachProd.id}> {eachProd.name}</h2>
-        })
-      } 
+          return <AdminRow key={eachProd.id} product={eachProd} />})
+        } 
+      </tbody>        
     </table>
   )
 }
