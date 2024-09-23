7bbd90bb49b6c007a6cb7881ae64a002f619a12d
ferchu <f3rn4nd4.2022@gmail.com>
2024-09-23 16:40:47 -0300

    registro

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -3,47 +3,48 @@ import { useForm } from "react-hook-form";
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
-const URL ="https://66e84eecb17821a9d9dc3463.mockapi.io/api/v1"
+const URL =import.meta.env.VITE_SERVER_URL
 
 export default function Register() {
 
-  // const [user, setUser] = useState([]);
+  const [user, setUser] = useState([]);
 
-  // const {register, handleSubmit, formState:{error,isValid}} = useForm();
+  const {register, reset, handleSubmit, formState:{errors,isValid}} = useForm();
 
-  // useEffect(() => {getUser();}, [])
+  useEffect(() => {getUser();}, [])
   
-  // async function getUser() {
-  //   try {
-  //     const response = await axios.get(`${URL}/register`);
-  //     setUser(response.data);
-  //     console.log(response.data)
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
+  async function getUser() {
+    try {
+      const response = await axios.get(`${URL}/register`);
+      setUser(response.data);
+      console.log(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
-  // async function newRegister(userName) {
-  //   console.log(userName)
-  //   try {
-  //     const response = await axios.post(`${URL}/register`, userName)
-  //     console.log(response.data)
-  //     getUser();
-  //   } catch (error) {
-  //     console.log(error)
+  async function newRegister(userName) {
+    console.log(userName)
+    try {
+      const response = await axios.post(`${URL}/register`, userName)
+      console.log(response.data)
+      getUser();
+      reset()
+    } catch (error) {
+      console.log(error)
       
-  //   }
-  // }
+    }
+  }
   return (
     <div className="register_container">
       <h2>REGISTRO DE NUEVO USUARIO</h2>
       <h3>Complete con sus datos personales</h3>
-      <form className="register_form" >
+      <form className="register_form"  onSubmit={handleSubmit(newRegister)}>
         <div className="input_container">
           <label htmlFor='fullname'>Nombre y Apellido </label>
           <input type="text" name="fullname" id="fullname"/>
-          {/* {errors.fullname?.type === "required" && <div className="input-error">El campo es requerido</div>}
-          {errors.fullname?.type === "minLength" && <div className="input-error">El mínimo de caracteres es 6</div>}    */}
+          {errors.fullname?.type === "required" && <div className="input-error">El campo es requerido</div>}
+          {errors.fullname?.type === "minLength" && <div className="input-error">El mínimo de caracteres es 6</div>}   
         </div>
         <div className="input_container">
           <label htmlFor="email">Correo electronico</label>
@@ -54,10 +55,10 @@ export default function Register() {
           <label htmlFor="password">Contraseña</label>
           <input type="password" name="password" id="password" required=""  minLength={6} maxLength={10}/>
         </div>
-        <div className="input_container">
+        {/* <div className="input_container">
           <label htmlFor="password">Repetir Contraseña</label>
           <input type="password" name="password" id="password" required="" />
-        </div>
+        </div> */}
         <div className="input_container">
           <label htmlFor="date_birth">Fecha de Nacimiento</label>
           <input type="date" name="date_birth" id="date_birth" required="" />
